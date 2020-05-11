89caedb207f1d90243306cc7ad4effb43fa220be
David Kang <davidwskang@gmail.com>
2020-05-11 05:52:34 -0400

    create basic splash screen using a fragment. Once splash screen completes, transition to home screen.

---
+++ app/src/main/java/com/davidwskang/memorymatchinggame/splash_screen/SplashScreenFragment.kt
@@ -0,0 +1,37 @@
+package com.davidwskang.memorymatchinggame.splash_screen
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import com.davidwskang.memorymatchinggame.MainActivity
+import com.davidwskang.memorymatchinggame.R
+
+
+class SplashScreenFragment: Fragment() {
+
+    companion object {
+        const val TAG = "splash"
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? = inflater.inflate(R.layout.fragment_splash_screen, container, false)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        activity?.run {
+            window.statusBarColor = ContextCompat.getColor(this, R.color.s_green)
+        }
+
+        view.postDelayed({
+            val ma = activity as MainActivity
+            ma.onSplashScreenComplete()
+        }, 2000)
+    }
+}
